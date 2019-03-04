b8c0168d26f905c79c82fb6db5864cec01e44339
Ciku Kariuki <wanjikukariuki03@gmail.com>
2019-03-04 14:22:09 +0300

    Added Js

--- js/pig.js
+++ js/pig.js
@@ -1,9 +1,29 @@
-function (){}
+$("document").ready(function(){
+    $(".p2").hide();
+  })
+
+  function Players(name){
+    this.name=name;
+  }
+  var start = function () {
+  var player1=$("play1").val();
+  var player2=$("play2").val();
+  if(player1 && player2){
+    document.write(player1,player2)
+    var playOne= new Player(player1);
+    var playTwo= new Player(player2);
+    $("#play1").text(playOne);
+    $("#play2").text(playTwo);
+    $(".p1").hide();
+    $(".p2").show();
+  }else{alert ("Please Enter Player Name")}
+}
 
 
-
-
-$("button#playbutton").click(function(){
-  var die=Math.floor(Math.random()*6) +1)
-  
-})
+// $("button#playbutton").click(function(){
+//   var dice=document.getElementById("dice").val();
+//   var status=document.getElementById("status").val();
+//   var die=Math.floor(Math.random()*6) +1)
+//
+//   dice.innerHTML=die;
+// })
